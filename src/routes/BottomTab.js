191736af265390daec9beefc19dcf312ac19c56e
iqbalnova <iqbalnova707@gmail.com>
2022-05-12 15:54:01 +0700

    added custom bottom tabs

--- src/routes/BottomTab.js
+++ src/routes/BottomTab.js
@@ -5,6 +5,8 @@ import React from 'react';
 import Feather from 'react-native-vector-icons/Feather';
 import QRCode from '../screens/QRCode';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Profile from '../screens/Profile';
+import {View} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,16 +14,29 @@ const BottomTab = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      activeColor="#fff"
-      inactiveColor="grey"
-      screenOptions={{headerShown: false}}
-      barStyle={{backgroundColor: '#694fad'}}>
+      activeColor="#5a5e64"
+      inactiveColor="#fff"
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 25,
+          left: 30,
+          right: 30,
+          elevation: 2,
+          backgroundColor: '#fff',
+          borderRadius: 25,
+          height: 60,
+          elevation: 0,
+        },
+      }}>
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
           tabBarIcon: ({color}) => {
-            return <Feather name="home" size={20} color={color} />;
+            return <Feather name="home" size={30} color={color} />;
           },
         }}
       />
@@ -32,12 +47,31 @@ const BottomTab = () => {
         options={{
           tabBarIcon: ({color}) => {
             return (
-              <MaterialCommunityIcons
-                name="qrcode-scan"
-                size={20}
-                color={color}
-              />
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  padding: 10,
+                  marginTop: -50,
+                  borderRadius: 20,
+                  elevation: 1,
+                }}>
+                <MaterialCommunityIcons
+                  name="qrcode-scan"
+                  size={40}
+                  color={color}
+                />
+              </View>
             );
+          },
+        }}
+      />
+
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({color}) => {
+            return <Feather name="user" size={30} color={color} />;
           },
         }}
       />
