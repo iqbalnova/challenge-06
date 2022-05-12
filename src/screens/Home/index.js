aac3d9dca70ddfb14d9867b40a3ec969e7892b2f
iqbalnova <iqbalnova707@gmail.com>
2022-05-12 18:33:34 +0700

    added geolocation for location

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import Geolocation from '@react-native-community/geolocation';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
+Geolocation.setRNConfiguration({
+  enableHighAccuracy: false,
+  timeout: 5000,
+  maximumAge: 10000,
+});
+
 const Home = () => {
+  const [pos, setPos] = useState({});
+  useEffect(() => {
+    Geolocation.getCurrentPosition(info => {
+      console.log(info);
+      setPos({
+        lat: info.coords.latitude,
+        long: info.coords.longitude,
+      });
+    });
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
@@ -16,12 +34,12 @@ const Home = () => {
             longitudeDelta: 0.0421,
           }}
           showsUserLocation={true}
-          customMapStyle={mapStyle}>
+          customMapStyle={mapDarkStyle}>
           <Marker
             draggable
             coordinate={{
-              latitude: -7.79285075950564,
-              longitude: 110.36558090019608,
+              latitude: pos.lat ?? -7.79285075950564,
+              longitude: pos.long ?? 110.36558090019608,
             }}
             onDragEnd={e => alert(JSON.stringify(e.nativeEvent.coordinate))}
             title={'Test Marker'}
@@ -35,84 +53,201 @@ const Home = () => {
 
 export default Home;
 
-const mapStyle = [
-  {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-  {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-  {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+const mapDarkStyle = [
+  {
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#212121',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#212121',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.country',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#9e9e9e',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
   {
     featureType: 'administrative.locality',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}],
+    stylers: [
+      {
+        color: '#bdbdbd',
+      },
+    ],
   },
   {
     featureType: 'poi',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}],
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
   },
   {
     featureType: 'poi.park',
     elementType: 'geometry',
-    stylers: [{color: '#263c3f'}],
+    stylers: [
+      {
+        color: '#181818',
+      },
+    ],
   },
   {
     featureType: 'poi.park',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#6b9a76'}],
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#1b1b1b',
+      },
+    ],
   },
   {
     featureType: 'road',
-    elementType: 'geometry',
-    stylers: [{color: '#38414e'}],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry.stroke',
-    stylers: [{color: '#212a37'}],
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#2c2c2c',
+      },
+    ],
   },
   {
     featureType: 'road',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#9ca5b3'}],
+    stylers: [
+      {
+        color: '#8a8a8a',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#373737',
+      },
+    ],
   },
   {
     featureType: 'road.highway',
     elementType: 'geometry',
-    stylers: [{color: '#746855'}],
+    stylers: [
+      {
+        color: '#3c3c3c',
+      },
+    ],
   },
   {
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [{color: '#1f2835'}],
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#4e4e4e',
+      },
+    ],
   },
   {
-    featureType: 'road.highway',
+    featureType: 'road.local',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#f3d19c'}],
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
   },
   {
     featureType: 'transit',
-    elementType: 'geometry',
-    stylers: [{color: '#2f3948'}],
-  },
-  {
-    featureType: 'transit.station',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}],
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
   },
   {
     featureType: 'water',
     elementType: 'geometry',
-    stylers: [{color: '#17263c'}],
+    stylers: [
+      {
+        color: '#000000',
+      },
+    ],
   },
   {
     featureType: 'water',
     elementType: 'labels.text.fill',
-    stylers: [{color: '#515c6d'}],
+    stylers: [
+      {
+        color: '#3d3d3d',
+      },
+    ],
   },
+];
+
+const mapStandardStyle = [
   {
-    featureType: 'water',
-    elementType: 'labels.text.stroke',
-    stylers: [{color: '#17263c'}],
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
   },
 ];
 
